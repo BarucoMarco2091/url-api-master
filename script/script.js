05cc0530c3e78571d062818fb6a00ef119f15012
BarucoMarco2091 <108827736+BarucoMarco2091@users.noreply.github.com>
2024-11-21 09:27:22 -0300

    api

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const shortenButton = document.querySelector('.shorten-btn');
+shortenButton.addEventListener('click', () => {
+    const urlInput = document.getElementById('url');
+    const shortUrlDiv = document.querySelector('.results');
+    const axios = require('axios');
+    async () => {
+        
+    }
+    
+});
